0e154ba4a06260ade8db2291bc42de4530e8b474
kylepunta <kylepurcell1998@gmail.com>
2024-12-29 19:58:14 +0000

    Created variables for temperature tick boxes and each temperature option; added click event listeners for each temperature setting to set and remove the active-button id attribute

--- src/index.js
+++ src/index.js
@@ -7,8 +7,12 @@ const closeBtn = document.querySelector('.close-button');
 const searchInput = document.querySelector('#location');
 const searchBox = document.querySelector('.search-box');
 const searchBtn = document.querySelector('#search-button');
+const tickBoxes = document.querySelectorAll('#tick-box');
+const fahrenheit = document.querySelector('.fahrenheit');
+const celsius = document.querySelector('.celsius');
 
 sidebar.classList.add('hidden');
+tickBoxes[1].setAttribute('id', 'active-setting');
 
 dropdownBtn.addEventListener('click', () => {
     sidebar.classList.toggle('hidden');
@@ -26,4 +30,13 @@ searchInput.addEventListener('focus', () => {
 searchInput.addEventListener('blur', () => {
     searchBox.classList.remove('active');
     searchBtn.removeAttribute('id', 'active-button');
+});
+
+fahrenheit.addEventListener('click', () => {
+    tickBoxes[0].setAttribute('id', 'active-button');
+    tickBoxes[1].removeAttribute('id', 'active-button');
+});
+celsius.addEventListener('click', () => {
+    tickBoxes[0].removeAttribute('id', 'active-button');
+    tickBoxes[1].setAttribute('id', 'active-button');
 });
