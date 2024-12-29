5ae7d843bf6dcaf7f1922e29da64b3bec1833d94
kylepunta <kylepurcell1998@gmail.com>
2024-12-29 17:51:23 +0000

    Created variables for search box, search input element and search button; added focus and blur event listeners to highlight the search box border and search button

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ console.log();
 const dropdownBtn = document.querySelector('.dropdown-button');
 const sidebar = document.querySelector('aside');
 const closeBtn = document.querySelector('.close-button');
+const searchInput = document.querySelector('#location');
+const searchBox = document.querySelector('.search-box');
+const searchBtn = document.querySelector('#search-button');
 
 sidebar.classList.add('hidden');
 
@@ -13,4 +16,14 @@ dropdownBtn.addEventListener('click', () => {
 
 closeBtn.addEventListener('click', () => {
     sidebar.classList.toggle('hidden');
+});
+
+searchInput.addEventListener('focus', () => {
+    searchBox.classList.add('active');
+    searchBtn.setAttribute('id', 'active-button');
+});
+
+searchInput.addEventListener('blur', () => {
+    searchBox.classList.remove('active');
+    searchBtn.removeAttribute('id', 'active-button');
 });
