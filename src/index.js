65e5a04286b3d0f1109f3e2875ea665069465fed
kylepunta <kylepurcell1998@gmail.com>
2024-12-31 22:06:25 +0000

    Imported renderUI IIFE; created variable for hour temperature divs and updated them with paragraph elements; invoked renderUI IIFE functions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./styles.css";
 import { eventHandler } from "./eventListeners.js";
+import { renderUI } from "./UI.js";
 
 const settings = document.querySelector("aside");
 const tickBoxes = document.querySelectorAll("#tick-box");
@@ -10,3 +11,28 @@ tickBoxes[1].setAttribute("id", "active-setting");
 eventHandler.addMenuBtnListeners();
 eventHandler.addSettingsListeners();
 eventHandler.addSearchListeners();
+
+// const hours = document.querySelectorAll(".hours > div");
+
+// hours[0].classList.add("first-hour");
+// hours[7].classList.add("last-hour");
+
+// for (let i = 0; i < 8; i++) {
+//   hours[i].setAttribute("id", "active-hour");
+// }
+
+const hourTemperatureContainers =
+  document.querySelectorAll(".hour-temperature");
+
+hourTemperatureContainers.forEach((container) => {
+  const temp = document.createElement("p");
+  temp.textContent = "4°";
+  container.appendChild(temp);
+});
+
+renderUI.renderHourlyWeather();
+eventHandler.addArrowListeners();
+renderUI.renderWeeklyWeather();
+renderUI.renderCurrentWeather();
+renderUI.renderWeeklyWeatherInfo();
+renderUI.renderWorldWeatherInfo();
